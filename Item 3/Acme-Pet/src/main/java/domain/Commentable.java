0e674aadf540a3fc66adf8330387881217e78062
Roldans <roldis2@hotmail.com>
2017-05-17 11:48:45 +0200

    problema comentando

--- Item 3/Acme-Pet/src/main/java/domain/Commentable.java
+++ Item 3/Acme-Pet/src/main/java/domain/Commentable.java
@@ -7,7 +7,7 @@ import javax.persistence.Entity;
 
 @Entity
 @Access(AccessType.PROPERTY)
-public class Commentable extends DomainEntity {
+public abstract class Commentable extends DomainEntity {
 
 	// Attributes -------------------------------------------------------------
 
