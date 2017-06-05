a4622c653b733e3051e76db250eeaded352b2e64
Unknown <trarsheise@gmail.com>
2017-06-05 02:56:35 +0200

    Cambiado proyecto nutch a java 1.7
    Por comodidad habia trabajado fuera de la mquiána virtual y al llevarlo dentro la dependencia maven de la librería any23 fallaba por la ersión de java. Mea culpa.

--- Item 10-Nutch/Nutch/src/main/java/dp/nutch/NutchMicrodataParser.java
+++ Item 10-Nutch/Nutch/src/main/java/dp/nutch/NutchMicrodataParser.java
@@ -1,3 +1,4 @@
+
 package dp.nutch;
 
 import java.io.ByteArrayOutputStream;
@@ -15,91 +16,83 @@ import org.apache.any23.extractor.microdata.ItemScope;
 import org.apache.any23.extractor.microdata.MicrodataParser;
 import org.w3c.dom.Document;
 
+public class NutchMicrodataParser {
 
+	public static void main(final String[] args) {
+		Collection<File> htmls;
+		Collection<ItemScope> itemScopes, all;
 
-public class NutchMicrodataParser 
-{
-    public static void main( String[] args ) {
-    	Collection<File> htmls;
-    	Collection<ItemScope> itemScopes;
-    	
-    	htmls = getFiles("resources/dump");
-    	itemScopes = new HashSet<ItemScope>();
-    	
-    	try {
-			for (File file : htmls){
-				for (ItemScope item : parseMicrodata(file)){
-					if (item.getType().toString().equals("http://schema.org/Product")){
+		htmls = NutchMicrodataParser.getFiles("resources/dump");
+		itemScopes = new HashSet<ItemScope>();
+		try {
+			for (final File file : htmls) {
+				all = NutchMicrodataParser.parseMicrodata(file);
+				for (final ItemScope item : all)
+					if (item.getType().toString().equals("http://schema.org/Product"))
 						itemScopes.add(item);
-					}
-				}
 			}
 			System.out.println(itemScopes.size());
-		} catch (IOException e) {
+		} catch (final IOException e) {
 			System.out.println(e.getStackTrace());
 		}
-    	
-    }
-    
-    private static Collection<ItemScope> parseMicrodata(File file) throws IOException {
-    	
-    	Document document;
-    	Collection<ItemScope> result;
-    	ItemScope[] itemScopes;
-    	
-    	document = getFileAsDocument(file);
 
-    	itemScopes = MicrodataParser.getMicrodata(document).getDetectedItemScopes();
-    	result = new HashSet<ItemScope>(Arrays.asList(itemScopes));
-    		
-    	return result;
-    
-    }
-    
-    private static String parseMicrodataAsJson(File file) throws IOException {
-    	
-    	Document document;
-    	ByteArrayOutputStream byteArrayOutput;
-    	String result;
-    	
-    	document = getFileAsDocument(file);
-    	byteArrayOutput = new ByteArrayOutputStream();
-    	
-    	MicrodataParser.getMicrodataAsJSON(document, new PrintStream(byteArrayOutput));
-    	
-    	result = byteArrayOutput.toString("UTF-8");
-    	return result;
-    
-    }
-    
-    private static Document getFileAsDocument(File file) throws IOException {
-    	
-    	InputStream documentInputInputStream;
-    	TagSoupParser tagSoupParser;
-    	Document result;
-    	
-    	documentInputInputStream = new FileInputStream(file);
-    	tagSoupParser = new TagSoupParser(documentInputInputStream, file.getAbsolutePath());
-    	result = tagSoupParser.getDOM();
-    	
-    	return result;
-    
-    }
+	}
+	private static Collection<ItemScope> parseMicrodata(final File file) throws IOException {
 
-	private static Collection<File> getFiles(String dir) {
+		Document document;
+		Collection<ItemScope> result;
+		ItemScope[] itemScopes;
+
+		document = NutchMicrodataParser.getFileAsDocument(file);
+
+		itemScopes = MicrodataParser.getMicrodata(document).getDetectedItemScopes();
+		result = new HashSet<ItemScope>(Arrays.asList(itemScopes));
+
+		return result;
+
+	}
+
+	private static String parseMicrodataAsJson(final File file) throws IOException {
+
+		Document document;
+		ByteArrayOutputStream byteArrayOutput;
+		String result;
+
+		document = NutchMicrodataParser.getFileAsDocument(file);
+		byteArrayOutput = new ByteArrayOutputStream();
+
+		MicrodataParser.getMicrodataAsJSON(document, new PrintStream(byteArrayOutput));
+
+		result = byteArrayOutput.toString("UTF-8");
+		return result;
+
+	}
+
+	private static Document getFileAsDocument(final File file) throws IOException {
+
+		InputStream documentInputInputStream;
+		TagSoupParser tagSoupParser;
+		Document result;
+
+		documentInputInputStream = new FileInputStream(file);
+		tagSoupParser = new TagSoupParser(documentInputInputStream, file.getAbsolutePath());
+		result = tagSoupParser.getDOM();
+		return result;
+
+	}
+
+	private static Collection<File> getFiles(final String dir) {
 		File[] files;
-    	Collection<File> result;
-    	
-    	result = new HashSet<File>();
+		Collection<File> result;
+
+		result = new HashSet<File>();
 		files = new File(dir).listFiles();
-		
-	    for (File file : files) {
-	        if (file.isDirectory()) {
-	            result.addAll(getFiles(file.getAbsolutePath()));
-	        } else {
-	            result.add(file);
-	        }
-	    }
-	    return result;
+
+		for (final File file : files)
+			if (file.isDirectory())
+				result.addAll(NutchMicrodataParser.getFiles(file.getAbsolutePath()));
+			else
+				result.add(file);
+		return result;
 	}
 }
